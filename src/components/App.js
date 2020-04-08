f2b123c2e0879343034e35a37301e44627813505
maisdev <maisiklar@yahoo.com>
2020-04-09 01:43:25 +0300

    continued tutorial. app component structure established

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+const App = () => {
+    return(
+        <div><SearchBar></SearchBar></div>
+    )
+}
+
+export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <input type="text"></input>
+                </form>
+            </div>
+        );
+    };
+};
+
+export default SearchBar;
+
+
